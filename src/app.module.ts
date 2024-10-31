70309cf7c96f8fdf88a4a1678bf121278a9b610f
Omar Guerra Alfaro Mac <omarrrrignacio@gmail.com>
2024-10-31 17:44:19 -0300

    prueba deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,11 @@ import { ApoderadoModule, EstudianteModule, ProfesorModule, CursoModule, Usuario
     PdfModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '44.207.168.60',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'colegio-2025',
+      username: 'colegioa_omarignacio',
+      password: '068gJOWTJ@@k',
+      database: 'colegioa_colegio3',
       entities,
       synchronize: true
     }),
@@ -54,7 +54,7 @@ export class AppModule { }
 //   port: 3306,
 //   username: 'root',
 //   password: '',
-//   database: 'colegio4',
+//   database: 'colegio-2025',
 //   entities,
 //   synchronize: true
 // }),
